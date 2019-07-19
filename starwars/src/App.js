a831b1ccaa2f1710241b03ba36dc847a1fec8033
danRolon <daniel.rolon@yahoo.com>
2019-07-19 11:54:42 -0500

    innitial commit - mvp ish

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import PersonCard from './components/personCard.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +14,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [people, setPeople] = useState([]);
+  const [pageNo, setPageNo] = useState('1');
+
+  useEffect(()=>{
+    axios.get(`https://swapi.co/api/people/?page=${pageNo}`)
+    .then(whatchuGet=>{
+      console.log(whatchuGet);
+      setPeople(whatchuGet.data.results);
+    })
+    .catch( err => {
+      console.log("Error:", err);
+    })    
+  },[pageNo]);
+
+
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {people.map((person,i)=>{
+        return <PersonCard data={person}/>
+      })}
     </div>
   );
 }
